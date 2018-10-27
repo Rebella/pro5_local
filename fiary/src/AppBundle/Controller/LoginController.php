9e628ce582c42b8c67cc85bd5be02f2624cc35b8
Isabella <s1610238056@students.fh-hagenberg.at>
2018-10-27 16:39:54 +0200

    umbenennen login conroller zum verlinken in nav

--- fiary/src/AppBundle/Controller/LoginController.php
+++ fiary/src/AppBundle/Controller/LoginController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends Controller
+class LoginController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/login", name="login")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/login.html.twig', [
+        return $this->render('pages/login.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
