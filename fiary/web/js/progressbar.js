62568bb244e94b25a4b23c616626bf46776d550e
nataschara <s1610238066@fhooe.at>
2018-11-06 16:06:04 +0100

    ProgressBarPart1

---
+++ fiary/web/js/progressbar.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Natascha on 06.11.2018.
+ */
+
+/* Erster Test der Animationen für die Progress Bar beim Eintrag erstellen */
+$(document).ready(function() {
+    $(".next-button").click(function() {
+        var current = $(this).parent();
+        var next = $(this)
+            .parent()
+            .next();
+        $(".progress li")
+            .eq($("fieldset").index(next))
+            .addClass("active");
+        current.hide();
+        next.show();
+    });
+
+    $(".prev-button").click(function() {
+        var current = $(this).parent();
+        var prev = $(this)
+            .parent()
+            .prev();
+        $(".progress li")
+            .eq($("fieldset").index(current))
+            .removeClass("active");
+        current.hide();
+        prev.show();
+    });
+});
+
